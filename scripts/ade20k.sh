d8e12a6cc4fed3222c00e7ba2904fb219be66808
seanseattle <seanseanttle@163.com>
2020-04-19 12:55:35 +0800

    change

--- scripts/ade20k.sh
+++ scripts/ade20k.sh
@@ -2,4 +2,4 @@
 #--gpu_ids 0,1,2,3 --ngf 64 --batchSize 4 --use_vae --niter 100 --niter_decay 100 --model smis --netE conv --netG ADE20K
 #
 python test.py --name ade20k_smis --dataset_mode ade20k --dataroot /home/zlxu/data/ADEChallengeData2016 --no_instance  \
---gpu_ids 0 --ngf 64 --batchSize 8 --use_vae --model smis --netE conv --netG ADE20K
+--gpu_ids 0 --ngf 64 --batchSize 2 --model smis --netG ADE20K
